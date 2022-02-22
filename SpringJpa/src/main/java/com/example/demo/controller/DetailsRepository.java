0fcd304ad808fcf59c0e505e501203c956195b3d
joel-varghese <66800039+joel-varghese@users.noreply.github.com>
2022-02-22 11:07:14 +0530

    Delete SpringJpa directory

--- SpringJpa/src/main/java/com/example/demo/controller/DetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.controller;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetailsRepository extends JpaRepository<Employdetails, Integer> {
-
-}
--- SpringJpa/src/main/java/com/example/demo/controller/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.controller;
-
-public enum Gender {
-	MALE, FEMALE
-}
